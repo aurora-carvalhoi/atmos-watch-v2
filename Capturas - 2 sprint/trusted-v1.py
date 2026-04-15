e017a604aae777e5327f26d2f00126007913896a
Kaio-rocha-sptech <kaio.rocha@sptech.school>
2026-04-14 21:00:33 -0300

    adição de nova coluna com nome da empresa

--- Capturas - 2 sprint/trusted-v1.py
+++ Capturas - 2 sprint/trusted-v1.py
@@ -2,6 +2,7 @@ import os
 import pandas as pd
 
 caminho_amostras = "./amostras"
+nome_empresa = "Atmos_teste"
 
 diretorio_trusted = os.path.join(caminho_amostras, "trusted")
 os.makedirs(diretorio_trusted, exist_ok=True)
@@ -19,6 +20,7 @@ for root, dirs, files in os.walk(caminho_amostras):
 
             try:
                 df = pd.read_csv(caminho_amostras)
+                df["empresa"] = nome_empresa
                 dataframe.append(df)
             except Exception as e:
                 print(f"Erro ao ler {caminho_amostras}: {e}")
